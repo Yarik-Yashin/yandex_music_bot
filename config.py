a14f63a75c3cf6128064b743f48b7dc4efa3ee00
Yaroslav <yarik101105@yandex.ru>
2020-12-25 22:30:33 +0300

    Created config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+from TOKEN import token
+
+settings = {
+    'token': token,
+    'bot': 'YAMB',
+    'id': '792071725071990784',
+    'prefix': '~'
+}
--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from yandex_music.client import Client
+from TOKEN import mail, password
+import discord
 
-client = Client.from_credentials('example@yandex.com', 'password')
+user = Client.from_credentials(mail, password)
 
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+
+client = MyClient()
+client.run('my token goes here')
